3de236943a301d5d3450a5b024af404ab1282259
David Danaher <Dafe@Davids-MacBook-Pro-2.local>
2016-04-26 20:31:18 -0500

    Initial Commit

---
+++ QuizMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuizMe
+//
+//  Created by David Danaher on 4/26/16.
+//  Copyright (c) 2016 Dafe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuizMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuizMe
+//
+//  Created by David Danaher on 4/26/16.
+//  Copyright (c) 2016 Dafe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
