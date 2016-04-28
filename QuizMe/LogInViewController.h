ebe634562e363977b5d7b58d429a74477ad7a95a
David Danaher <Ddanaher@wisc.edu>
2016-04-28 02:06:24 -0500

    Added Navigation bar

---
+++ QuizMe/LogInViewController.h
@@ -0,0 +1,13 @@
+//
+//  LogInViewController.h
+//  
+//
+//  Created by David Danaher on 4/27/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInViewController : UIViewController
+
+@end
---
+++ QuizMe/SignUpViewController.m
@@ -0,0 +1,43 @@
+//
+//  SignUpViewController.m
+//  
+//
+//  Created by David Danaher on 4/27/16.
+//
+//
+
+#import "SignUpViewController.h"
+
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+@interface SignUpViewController ()
+
+@end
+
+@implementation SignUpViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    // Optional: Place the button in the center of your view.
+    loginButton.center = self.view.center;
+    [self.view addSubview:loginButton];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
